50252344c5d17d09ae5aa35c5aab9e9d84942bc7
Ammar Sahyoun <ammarzgr@hotmail.com>
2024-08-14 10:02:24 +0200

    Adding inventory

---
+++ models/staging/stg_northwind__inventory.sql
@@ -0,0 +1,41 @@
+{{ config(materialized='incremental', unique_key='inventory_id')}}
+
+with ranked_src as(
+select 
+	*,
+	row_number() over (partition by id, product_id, purchase_order_id order by transaction_created_date, transaction_modified_date desc) as rnk
+from {{ source('dbo','inventory_transactions') }}
+),
+
+
+latest as(
+  select
+   * 
+  from ranked_src where rnk = 1
+)
+ 
+
+select 
+  id as inventory_id, 
+  case
+    when transaction_type = 1 then 'Purchased'
+    when transaction_type = 2 then 'Sold'
+    when transaction_type = 3 then 'On Hold'
+    when transaction_type = 4 then 'Waste'
+  end as transaction_name,
+  transaction_created_date::date, 
+  transaction_modified_date::date, 
+  product_id, 
+  quantity, 
+  purchase_order_id, 
+  customer_order_id, 
+  comments,
+  '{{ invocation_id }}'  as batch_id,
+  'db' as source_data,
+  current_timestamp::timestamp(0) as load_dt
+from latest 
+
+  
+{% if is_incremental() %}
+    where current_timestamp > ( select max(load_dt) from {{this}} )
+{% endif %}
