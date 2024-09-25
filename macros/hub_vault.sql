e3bac116f7a34d4ecb7dca502d115e06d2c9ffa5
Ammar Sahyoun <ammarzgr@hotmail.com>
2024-09-25 22:41:48 +0200

    Adding incremental to hub macro

--- macros/hub_vault.sql
+++ macros/hub_vault.sql
@@ -1,34 +1,48 @@
-{% macro hub(metadata, md_batch, md_load_dt) %}
+{% macro hub(metadata) %}
 
-  {# Extract target-related metadata #}
   {%- set target_metadata = metadata['target'] if 'target' in metadata else {} %}
   {%- set hub_key = target_metadata.get('hub_key', '') %}
   {%- set natural_key = target_metadata.get('natural_key', '') %}
+  {%- set record_source = target_metadata.get('record_source', '') %}
 
-  {# Extract source-related metadata #}
   {%- set sources_metadata = metadata['sources'][0] if 'sources' in metadata and metadata['sources']|length > 0 else {} %}
   {%- set source_model = sources_metadata.get('model_name', '') %}
-  {%- set md_record_source = metadata.get('md_record_source', 'unknown') %}
 
-  {# Fallback logic if no source_model is defined #}
   {% if source_model == '' %}
-    {{ exceptions.raise_compiler_error("No source model defined in metadata.") }}
+    {{ exceptions.raise_compiler_error("No source model defined in metadata!") }}
   {% endif %}
 
-  {# Ensure md_batch and md_load_dt are treated as strings #}
-  {%- set md_batch_str = "'" ~ md_batch ~ "'" %}
-  {%- set md_load_dt_str = "'" ~ md_load_dt ~ "'" %}
+  {{ config(materialized='incremental', unique_key = natural_key)}}
 
-  {# Generate hub table with hashed key #}
-  with hub_data as (
+  with hash_key as (
     select
       {{ hashed_key(natural_key) }} as {{ hub_key }},
-      '{{ md_record_source }}' as md_record_source,
-      {{ md_batch_str }} as md_batch,
-      {{ md_load_dt_str }} as md_load_dt
+      {{ natural_key }},
+      load_dt
     from {{ source_model }}
+  ),
+
+  hub as (
+    select
+      {{ hub_key }},
+      {{ natural_key }},  -- Ensure natural_key (customer_id) is selected
+      load_dt,
+      load_dt as md_load_dt,
+      '{{ record_source }}' as md_record_source,
+      '{{ invocation_id }}' as md_batch
+    from hash_key
   )
 
-select * from hub_data
+select 
+  {{ hub_key }},
+  {{ natural_key }},  -- Include natural_key for use in deletion
+  md_record_source,
+  md_batch,
+  md_load_dt
+from hub
+
+{% if is_incremental() %}
+  where load_dt > (select max(md_load_dt::timestamp) from {{this}})
+{% endif %}
 
 {% endmacro %}
--- models/vault/customer/vlt__h_customer_main.sql
+++ models/vault/customer/vlt__h_customer_main.sql
@@ -2,12 +2,14 @@
 target:
   hub_key: h_customer_key
   natural_key: customer_id
+  record_source: staging
+  
 sources:
   - model_name: staging.stg_northwind__customers
     natural_keys:
       - customer_id
 
-md_record_source: staging
+
 {%- endset %}
 
-{{ hub(fromyaml(metadata_hub), invocation_id, run_started_at) }}
+{{ hub(fromyaml(metadata_hub)) }}
